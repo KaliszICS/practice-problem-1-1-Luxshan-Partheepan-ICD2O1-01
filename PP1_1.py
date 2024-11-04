af2f6e0af36c0b595a734e9a9bd94d66ecc2caf2
Luxshan-Partheepan-ICD2O1-01 <luxshan.partheepan@student.tdsb.on.ca>
2024-11-04 09:20:12 -0500

    Update PP1_1.py

--- PP1_1.py
+++ PP1_1.py
@@ -1,15 +1,19 @@
 def q1():
-  #Write Assignment code here
+  print("Hello World")
 def q2():
-  #Write Assignment code here
+  print(1)
+  print(2)
+  print(3)
+  print(4)
+  print(5)
 
 def q3():
-  #Write Assignment code here
+  print("Help me!")
 
 def q4():
-  #Write Assignment code here
+  print(3 * 3 = 9)
 
-q1()
-q2()
-q3()
-q4()
+#q1()
+#q2()
+#q3()
+#q4()
